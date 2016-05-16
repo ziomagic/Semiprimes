5520fc71b190eb1bde8a20c1eaf12adc3042321b
Agnieszka <tynor.t@wp.pl>
2016-05-16 09:40:42 +0200

    Semi prime support

--- SemiPrime/SemiPrimes.cs
+++ SemiPrime/SemiPrimes.cs
@@ -36,6 +36,63 @@ namespace SemiPrime
             }
         }
 
+        /// <summary>
+        /// It will find Semi prime number from specific range
+        /// 
+        /// Example:
+        /// GetSemiPrime(1, 26) Will return 10
+        /// </summary>
+        /// <param name="from">Range start</param>
+        /// <param name="to">Range end</param>
+        /// <returns>Semiprime number, or -1 if semiprime not found</returns>
+        public int GetSemiPrime(int from, int to)
+        {
+            for (int i = from; i < to; i++)
+            {
+                if(isSemiPrime(i))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
 
+        /// <summary>
+        /// Get list of all semi primes between Q, and P
+        /// </summary>
+        /// <returns>List of semiprimes</returns>
+        public List<int> GetSemiPrimeList()
+        {
+            List<int> output = new List<int>();
+            for(int i = 0; i < P.Length; i++)
+            {
+                int semi = GetSemiPrime(P[i], Q[i]);
+                if(semi > -1)
+                {
+                    output.Add(semi);
+                }
+            }
+
+            return output;
+        }
+        /// <summary>
+        /// Function will check if specific number is semiprime
+        /// </summary>
+        /// <param name="c"></param>
+        /// <returns>Number semiprime status</returns>
+        bool isSemiPrime(int c)
+        {
+            int a = 2, b = 0;
+            while (b < 3 && c != 1)
+            {
+                if ((c % a) == 0)
+                {
+                    c /= a; b++;
+                }
+                else
+                    a++;
+            }
+            return b == 2;
+        }
     }
 }
